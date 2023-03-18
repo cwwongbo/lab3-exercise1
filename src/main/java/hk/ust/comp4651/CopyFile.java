c154e6d7b43c0428b33fa8493aecb592d9421815
cwwongbo <125867704+cwwongbo@users.noreply.github.com>
2023-03-18 18:40:06 +0800

    Copied from lab demo

--- src/main/java/hk/ust/comp4651/CopyFile.java
+++ src/main/java/hk/ust/comp4651/CopyFile.java
@@ -45,7 +45,19 @@ public class CopyFile {
         FSDataOutputStream out = null;
 
         // TODO: Your implementation goes here...
-        
+        in = inFS.open(new Path(src));
+        out = outFS.creat(new Path(dst)),
+                new Progressable(){
+                    /*
+                    * Print a dot whenever 64 KB of data has been written to 
+                    * the datanode pipeline
+                    */
+                    public void progress(){
+                        System.out.print(s:".");
+                    }
+                };
+        System.out.print(s:"\n");
+        IOUtils.copyBytes(in, out, BufferSize, close:true); 
     }
 
 }
